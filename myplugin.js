f9c70b00449938822d2bbc7c79bde5826a9d3016
Pauline Korkina <yuzha@inbox.ru>
2017-09-08 12:46:43 +0500

    Исправила багу с ускорением автолисталки после нажатия на кнопки

--- myplugin.js
+++ myplugin.js
@@ -1,63 +1,67 @@
 (function( $ ) {
   $.fn.myPlugin = function() {
 
-      var sliderUl = this.children('ul');
-      var sliderLi = sliderUl.children('li');
-      sliderUl.wrap('<div class="myPluginWrapper" />');
-      var prevControl = this.children('.prev');
-      var nextControl = this.children('.next');
-      var slideWidth = sliderLi.children().width();
-      var slideHeight = sliderLi.children().outerHeight();
-      var sliderLength = sliderLi.length;
-      var currentPosition = 0;
-      var autoSlide = false;
+    var sliderUl = this.children('ul');
+    var sliderLi = sliderUl.children('li');
+    sliderUl.wrap('<div class="myPluginWrapper" />');
+    var prevControl = this.children('.prev');
+    var nextControl = this.children('.next');
+    var slideWidth = sliderLi.children().width();
+    var slideHeight = sliderLi.children().outerHeight();
+    var sliderLength = sliderLi.length;
+    var currentPosition = 0;
+    var sliderWrapper = this.children('.myPluginWrapper');
+    sliderWrapper.width(slideWidth).height(slideHeight);
 
-      var sliderWrapper = this.children('.myPluginWrapper');
-      sliderWrapper.width(slideWidth).height(slideHeight);
+    sliderUl.width(slideWidth * sliderLength);
+    sliderUl.css({
+    	'position': 'absolute',
+    	'top': '0',
+    	'left': '0'
+    });
 
-      sliderUl.width(slideWidth * sliderLength);
-      sliderUl.css({
-      	'position': 'absolute',
-      	'top': '0',
-      	'left': '0'
-      });
+    var runInterval = setTimeout(runAuto, 2600);
 
-      function changeLeft() {
-      	var newLeft = currentPosition*slideWidth*(-1);
-      	sliderUl.animate({left: newLeft}, 600, function(){setInterval(runAuto, 2600)});
+    function changeLeft() {
+    	var newLeft = currentPosition*slideWidth*(-1);
+    	sliderUl.animate(
+        {left: newLeft},
+        600,
+        function(){
+            runInterval = setTimeout(runAuto, 2600)
+        });
       }
 
-      nextControl.click(function(){
-      	clearInterval(runInterval);
-      	if (currentPosition < sliderLength - 1) {
-      		currentPosition++;
-      	} else {
-      		currentPosition = 0;
-      	}
-      	changeLeft();
-      });
+    nextControl.click(function(){
+    	clearTimeout(runInterval);
+    	if (currentPosition < sliderLength - 1) {
+    		currentPosition++;
+    	} else {
+    		currentPosition = 0;
+    	}
+    	changeLeft();
+    });
 
-      prevControl.click(function(){
-      	clearInterval(runInterval);
-      	if (currentPosition > 0) {
-      		currentPosition--;
-      	} else {
-      		currentPosition = sliderLength - 1;
-      	}
-      	changeLeft();
-      });
+    prevControl.click(function(){
+    	clearTimeout(runInterval);
+    	if (currentPosition > 0) {
+    		currentPosition--;
+    	} else {
+    		currentPosition = sliderLength - 1;
+    	}
+    	changeLeft();
+    });
 
-      function runAuto() {
-      	if (currentPosition < sliderLength - 1) {
-      		currentPosition++;
-      	} else {
-      		currentPosition = 0;
-      	}
-      	var newLeft = currentPosition*slideWidth*(-1);
-      	sliderUl.animate({left: newLeft}, 600);
-      }
-
-      var runInterval = setInterval(runAuto, 2600);
+    function runAuto() {
+    	if (currentPosition < sliderLength - 1) {
+    		currentPosition++;
+    	} else {
+    		currentPosition = 0;
+    	}
+    	var newLeft = currentPosition*slideWidth*(-1);
+    	sliderUl.animate({left: newLeft}, 600);
+    	runInterval = setTimeout(runAuto, 2600);
+    }
 
   };
 })(jQuery);
